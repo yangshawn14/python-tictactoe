dc8a9ccb7b052810cb3d0579bdb49352e37dcc68
Shawn Yang <yangshawn14@gmail.com>
2023-01-25 11:55:43 -0700

    Update is_Tie function

--- main.py
+++ main.py
@@ -11,21 +11,25 @@ board = [1,2,3,
          4,5,6,
          7,8,9]
 winner=""
-currentPlayer = "X" 
-playingGame = True
+currentPlayer = "X"
+haveWinner = False
+gameDone = False
+
 
 
 
 
 def main():
     # Print board
-    while playingGame or is_Tie(board):
+    while not (gameDone or is_Tie(board)):
         displayBoard(board)
         playerInput(board, currentPlayer)
         checkForWin(board)
-        switchPlayer()
-    print(f"{winner} is the winner.")
-    print("Good game. Thanks for playing.")
+    if haveWinner:
+        print(f"{winner} is the winner.")
+        print("Good game. Thanks for playing!")
+    else:
+        print("It is a tie. Thanks for playing!")
     
 
 def displayBoard(board):
@@ -39,9 +43,14 @@ def displayBoard(board):
     
 
 def playerInput(board, currentPlayer):
+    # Set winner to the current player
+    global winner
+
     square = int(input(f"{currentPlayer}'s turn to choose a square(1-9): "))
     if square >= 1 and square <= 9 and square == board[square - 1]:
         board[square - 1] = currentPlayer
+        winner = currentPlayer
+        switchPlayer()
 
     else:
         print("Square is taken already. Try Again.")
@@ -49,15 +58,16 @@ def playerInput(board, currentPlayer):
 
 def switchPlayer():
     global currentPlayer
+
     if currentPlayer == "X":
         currentPlayer = "O"
     else:
         currentPlayer ="X"
 
 def checkForWin(board):
-    global currentPlayer
-    global winner
-    global playingGame
+    
+    global gameDone
+    global haveWinner
 
     if board[0] == board[1] == board[2] or \
        board[3] == board[4] == board[5] or \
@@ -67,17 +77,20 @@ def checkForWin(board):
        board[2] == board[5] == board[8] or \
        board[0] == board[4] == board[8] or \
        board[2] == board[4] == board[6]:
+
        displayBoard(board)
-       # Set winner to the current player
-       winner = currentPlayer
-       playingGame = False
+       gameDone = True
+       haveWinner = True
+    
+    
 
 def is_Tie(board):
     for i in range(9):
-        if board[i] != "X" or board[i] != "O":
+        if board[i] != "X" and board[i] != "O":
             return False
-        else:
-            return True
+    return True            
+            
+        
     
     
 
